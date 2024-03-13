d5c712c6fe9aeb164e26779e6edc829747165555
Abdul Majid <116735769+AbdulMajid226@users.noreply.github.com>
2024-03-13 11:10:00 +0700

    Add files via upload

---
+++ Pertemuan 4/org/poligon/Poligon.java
@@ -0,0 +1,13 @@
+//File : Poligon.java 13/3/2024
+//penulis : Abdul Majid
+//Deskripsi : representasi dasar dari objek poligon(segi banyak)
+
+package org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
