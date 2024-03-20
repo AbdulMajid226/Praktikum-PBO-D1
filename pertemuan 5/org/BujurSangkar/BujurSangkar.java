8db2e009f537fcd63e3c200ec5c9be6f0ef1bea0
Abdul Majid <116735769+AbdulMajid226@users.noreply.github.com>
2024-03-20 10:59:38 +0700

    Add files via upload

---
+++ pertemuan 5/org/BujurSangkar/BujurSangkar.java
@@ -0,0 +1,11 @@
+public class BujurSangkar extends BangunDatar{
+    /*public double hitungLuas(double sisi){
+        luas = sisi*sisi;
+        return luas;
+    }*/
+}
+//Apa yang terjadi apablia kelas BujurSangkar tidak membuat implementasi
+//metode abstrak yang ada pada kelas BangunDatar ? jelaskan !
+
+//Jawab : The type BujurSangkar must implement the inherited abstract method BangunDatar.hitungLuas(double),
+//        karena jika subclass dari class abstrak adalah class biasa maka class tersebut harus mengimplementasi semua method abstrak dari superclassnya
